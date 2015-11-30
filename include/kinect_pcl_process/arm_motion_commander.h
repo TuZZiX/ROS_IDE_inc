7b4034afa74a7c8300e3e701da259d5afc4d88f0
TuZZiX <tianshipei5@126.com>
2015-11-30 02:13:02 -0500

    update

--- include/kinect_pcl_process/arm_motion_commander.h
+++ include/kinect_pcl_process/arm_motion_commander.h
@@ -1,7 +1,3 @@
-// example_baxter_cart_move_action_client:
-// wsn, Nov, 2015
-// illustrates use of baxter_cart_move_as, action server called "cartMoveActionServer"
-
 #ifndef ARM_MOTION_COMMANDER_H_
 #define ARM_MOTION_COMMANDER_H_
 
@@ -26,8 +22,6 @@ private:
 	//messages to send/receive cartesian goals / results:
 	cwru_action::cwru_baxter_cart_moveGoal cart_goal_;
 	cwru_action::cwru_baxter_cart_moveResult cart_result_;
-	std::vector <double> q_vec_; //holder for right-arm angles
-	geometry_msgs::PoseStamped tool_pose_stamped_;
 	//an action client to send goals to cartesian-move action server
 	actionlib::SimpleActionClient<cwru_action::cwru_baxter_cart_moveAction> cart_move_action_client_; //("cartMoveActionServer", true);
 	double computed_arrival_time_;
@@ -40,22 +34,18 @@ public:
 	
 	~ArmMotionCommander(void) {
 	}
-	void send_test_goal(void);
-	int plan_move_to_pre_pose(void);
-	int rt_arm_execute_planned_path(void);
-	int rt_arm_request_q_data(void);
-	int rt_arm_request_tool_pose_wrt_torso(void);
-	geometry_msgs::PoseStamped get_rt_tool_pose_stamped(void) { return tool_pose_stamped_;};
+	bool moveArmsBack(void);
 	
-	Eigen::VectorXd get_right_arm_joint_angles(void);
-	int rt_arm_plan_jspace_path_current_to_qgoal(Eigen::VectorXd q_des_vec);
-	int rt_arm_plan_path_current_to_goal_pose(geometry_msgs::PoseStamped des_pose);
-	int rt_arm_plan_path_current_to_goal_dp_xyz(Eigen::Vector3d dp_displacement);
+	Eigen::VectorXd getJointAngles(void);
+	geometry_msgs::PoseStamped getGripperPose(void);
 	
-	//utilities to convert between affine and pose
-	Eigen::Affine3d transformPoseToEigenAffine3d(geometry_msgs::Pose pose);
-	geometry_msgs::Pose transformEigenAffine3dToPose(Eigen::Affine3d e);
-	void conv_to_pose(std::vector<geometry_msgs::PoseStamped> &pose_seq, std::vector<Eigen::Vector3f> &position_seq, Eigen::Quaterniond &orientation);
+	bool planPath(geometry_msgs::PoseStamped pose);
+	bool planPath(Eigen::VectorXd joints);
+	bool planPath(Eigen::Vector3d dp_displacement);
+	
+	bool executePath(double timeout = 0.0);
+	
+	void convToPose(std::vector<geometry_msgs::PoseStamped> &pose_seq, std::vector<Eigen::Vector3f> &position_seq, Eigen::Quaterniond &orientation);
 };
 
 #endif  // this closes the header-include trick...ALWAYS need one of these to match #ifndef
